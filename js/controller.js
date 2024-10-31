d61c7fa9073ec9ddd23dcfe972f9af4ce11e750d
Rinat Sarmuldin <ura07srr@gmail.com>
2024-10-31 16:01:02 +0500

    Modules Listening to form submissions

---
+++ js/controller.js
@@ -0,0 +1,10 @@
+var controller = (function (budgetCtrl, uiCtrl) {
+
+    function ctrlAddItem (event){
+        event.preventDefault();
+        console.log("Fired!");
+    }
+
+    document.querySelector("#budget-form").addEventListener('submit', ctrlAddItem);
+
+})(modelController, viewController);
