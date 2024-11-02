f3c5c829f0c195db2cd3100d93abb21a07a65670
Rinat Sarmuldin <ura07srr@gmail.com>
2024-11-02 11:28:59 +0500

    Retrieving data from a form

--- js/controller.js
+++ js/controller.js
@@ -1,10 +1,26 @@
 var controller = (function (budgetCtrl, uiCtrl) {
 
+    var DOM = uiCtrl.getDomStrings();
+
+    // Функция срабатывающая при отправке формы
     function ctrlAddItem (event){
         event.preventDefault();
         console.log("Fired!");
+
+        // 1. Получить данные из формы
+        var input = uiCtrl.getInput();
+        console.log(input);
+
+        // 2. Добавить полученные данные в модель
+
+        // 3. Добавить "запись" в UI
+
+        // 4. Посчитать бюджет
+
+        // 5. Отобразить бюджет в UI
+
     }
 
-    document.querySelector("#budget-form").addEventListener('submit', ctrlAddItem);
+    document.querySelector(DOM.form).addEventListener('submit', ctrlAddItem);
 
 })(modelController, viewController);
--- js/view.js
+++ js/view.js
@@ -1,3 +1,23 @@
 var viewController = (function () {
 
+    var DOMstrings = {
+        inputType: "#input__type",
+        inputDescription: "#input__description",
+        inputValue: "#input__value",
+        form: "#budget-form"
+    }
+
+    return {
+        getInput: function(){
+            return {
+                type: document.querySelector(DOMstrings.inputType).value,
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value,
+            }
+        },
+        getDomStrings: function(){
+            return DOMstrings
+        }
+    }
+
 })();
